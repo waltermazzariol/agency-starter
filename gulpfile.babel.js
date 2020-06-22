c73611943636d9b8a7074bc0c36624c558009aad
Walter Mazzariol <mazzariolwalter@gmail.com>
2020-06-23 00:46:38 +0200

    Initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,110 @@
+import webpack from 'webpack-stream';
+import { src, dest, watch, series, parallel } from 'gulp';
+import browserSync from "browser-sync";
+import replace from "gulp-replace";
+import zip from "gulp-zip";
+import info from "./package.json";
+import wpPot from "gulp-wp-pot";
+import del from 'del';
+import imagemin from 'gulp-imagemin';
+import yargs from 'yargs';
+import sass from 'gulp-sass';
+import cleanCss from 'gulp-clean-css';
+import gulpif from 'gulp-if';
+import postcss from 'gulp-postcss';
+import sourcemaps from 'gulp-sourcemaps';
+import autoprefixer from 'autoprefixer';
+import concat from 'gulp-concat'
+
+const PRODUCTION = yargs.argv.prod;
+const server = browserSync.create();
+
+export const serve = done => {
+  server.init({
+    proxy: "http://localhost:8080/starter/" // put your local website link here
+  });
+  done();
+};
+
+export const reload = done => {
+  server.reload();
+  done();
+};
+
+export const styles = () => {
+  return src('src/scss/bundle.scss')
+  .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulpif(PRODUCTION, postcss([ autoprefixer ])))
+  .pipe(gulpif(PRODUCTION, cleanCss({compatibility:'ie8'})))
+  .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+  .pipe(dest('dist/css'))
+  .pipe(browserSync.stream());
+}
+
+export const watchForChanges = () => {
+  watch('src/scss/**/*.scss', styles);
+  watch('src/images/**/*.{jpg,jpeg,png,svg,gif}', series(images, reload));
+  watch(['src/**/*','!src/{images,js,scss}','!src/{images,js,scss}/**/*'], series(copy, reload));
+  watch('src/js/**/*.js', series(scripts, reload));
+  watch("**/*.php", reload);
+}
+
+export const images = () => {
+  return src('src/images/**/*.{jpg,jpeg,png,svg,gif}')
+    .pipe(gulpif(PRODUCTION, imagemin()))
+    .pipe(dest('dist/images'));
+}
+
+export const copy = () => {
+  return src(['src/**/*','!src/{images,js,scss}','!src/{images,js,scss}/**/*'])
+    .pipe(dest('dist'));
+}
+
+export const clean = () => del(['dist']);
+
+export const scripts = () => {
+  return src([
+      'src/assets/vendor/jquery.js',
+      'src/assets/vendor/popper.js',
+      'src/assets/vendor/bootstrap.js',
+      'src/assets/js/*'
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(concat('bundle.js'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(dest('dist/js'))
+    .pipe(browserSync.stream());
+}
+
+export const compress = () => {
+  return src([
+      "**/*",
+      "!node_modules{,/**}",
+      "!bundled{,/**}",
+      "!src{,/**}",
+      "!.babelrc",
+      "!.gitignore",
+      "!gulpfile.babel.js",
+      "!package.json",
+      "!package-lock.json",
+    ])
+    .pipe(replace("_themename", info.name))
+    .pipe(zip(`${info.name}.zip`))
+    .pipe(dest('bundled'));
+  };
+
+  export const pot = () => {
+    return src("**/*.php")
+    .pipe(
+        wpPot({
+          domain: "_themename",
+          package: info.name
+        })
+      )
+    .pipe(dest(`languages/${info.name}.pot`));
+  };
+
+export const dev = series(clean, parallel(styles, images, copy, scripts), serve, watchForChanges);
+export const build = series(clean, parallel(styles, images, copy, scripts), pot, compress);
+export default dev;
---
+++ header.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package wordpress_gulp_starter
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<?php wp_body_open(); ?>
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'wordpress_gulp_starter'); ?></a>
+		<header class="site-header">
+			<nav class="navbar navbar-expand-md navbar-light bg-white fixed-top" id="mainNav">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<a class="navbar-brand" href="<?php bloginfo('url') ?>">
+						<?php
+						if (has_custom_logo()) {
+							the_custom_logo();
+						} else {
+							bloginfo('name');
+						}
+						?></a>
+					<?php
+					wp_nav_menu(array(
+						'theme_location'    => 'menu-1',
+						'depth'             => 2,
+						'container'         => 'div',
+						'container_class'   => 'collapse navbar-collapse',
+						'container_id'      => 'bs-example-navbar-collapse-1',
+						'menu_class'        => 'nav navbar-nav ml-auto align-items-center',
+						'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+						'walker'            => new WP_Bootstrap_Navwalker(),
+					));
+					?>
+				</div>
+			</nav>
+		</header><!-- #masthead -->
