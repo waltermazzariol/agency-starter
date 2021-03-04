c6cbd0858a03d556e64d4d1fa8b570d87763bdfa
Walter Mazzariol <mazzariolwalter@gmail.com>
2021-03-04 09:36:13 +0100

    fix navabar position mobile and input for cform7

--- header.php
+++ header.php
@@ -52,7 +52,7 @@
 						'container'         => 'div',
 						'container_class'   => 'collapse navbar-collapse',
 						'container_id'      => 'bs-example-navbar-collapse-1',
-						'menu_class'        => 'nav navbar-nav ml-auto align-items-center wow',
+						'menu_class'        => 'nav navbar-nav ml-auto wow',
 						'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
 						'walker'            => new WP_Bootstrap_Navwalker(),
 					));
