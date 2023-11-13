115c902f94b7cebf6a1b4adb169537f599c24722
Walter Mazzariol <mazzariolwalter@gmail.com>
2023-11-13 18:56:03 +0100

    add change image aspect ratio

--- functions.php
+++ functions.php
@@ -174,13 +174,13 @@ add_action('wp_enqueue_scripts', 'wp_guarapo_scripts');
 /**
  * Enqueue scripts and styles from dist.
  */
-function _themename_assets()
+function wp_guarapo_assets()
 {
-	wp_enqueue_style('_themename-stylesheet', get_template_directory_uri() . '/dist/css/bundle.css', array(), '1.0.0', 'all');
+	wp_enqueue_style('wp_guarapo-stylesheet', get_template_directory_uri() . '/dist/css/bundle.css', array(), '1.0.0', 'all');
 
-	wp_enqueue_script('_themename-scripts', get_template_directory_uri() . '/dist/js/bundle.js', array(), '1.0.0', true);
+	wp_enqueue_script('wp_guarapo-scripts', get_template_directory_uri() . '/dist/js/bundle.js', array(), '1.0.0', true);
 }
-add_action('wp_enqueue_scripts', '_themename_assets');
+add_action('wp_enqueue_scripts', 'wp_guarapo_assets');
 
 /**
  * Register Custom Navigation Walker
